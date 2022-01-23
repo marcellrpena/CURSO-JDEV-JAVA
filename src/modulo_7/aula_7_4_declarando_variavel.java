0a8a2bca020486d6998a0238225bd50c52e05372
AVELLG1713MAX <AVELLG1713MAX@MarcellRP.mshome.net>
2022-01-23 15:46:29 -0300

    aula 7.4 adição dos tipos de variáveis e suas formas de declaração

--- src/modulo_7/aula_7_4_declarando_variavel.java
+++ src/modulo_7/aula_7_4_declarando_variavel.java
@@ -4,6 +4,28 @@ public class aula_7_4_declarando_variavel {
 	
 	public static void main(String[] args) {
 		
+		boolean logica =true ; /* variavel do tipo logica recebe valores true ou false*/
 		
+		/* variavel do tipo numérica , recebe apenas números são 4 tipos com diferentes tamanhos
+		 * byte, short, int, long */
+		byte inteiro8bits = 8;
+		short inteiro16bits = 16;
+		int inteiro32bits = 32;
+		long inteiro64bits = 64;
+		
+		/*Variável do tipo String recebe 1 ou mais caracteres dentro de aspas duplas*/
+		String caractere = "string";
+		
+		/*Variável numérica do tipo double e float recebe números com virgula*/		
+		float decimal8digitos = 0.12345678f;
+		double dedcimal10digitos = 0.12345678910;
+		
+		/* variáveis do tipo "var" assumem qualquer tipo primitivo , após receber o valor o tipo
+		 * não se altera mais*/
+		var tomaForma = "RECEBE: NUMEROS, BOOLEAN, CARACTERES , ETC...." ;
+		
+		/*OBESERVAÇÃO:
+		 * PARA DECLARAR VARIAVEIS A PRIMEIRA LETRA SEMPRE É MINÚSCULA, CASO EXISTA 2 OU MAIS NOMES 
+		 * A PRIMEIRA LETRA DE CADA NOME SEMPRE SERÁ MAIÚSCULA O RESTO MINÚSCULA*/
 	}
 }
