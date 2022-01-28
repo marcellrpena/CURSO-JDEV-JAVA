1b63c5c52964eb52bc5c4a85c1f00e16fbea2cc9
AVELLG1713MAX <AVELLG1713MAX@MarcellRP.mshome.net>
2022-01-28 19:13:56 -0300

    adicionando a aula 7.8 tipo CHAR

---
+++ src/modulo_7/Aula7_8_tipo_Char.java
@@ -0,0 +1,18 @@
+package modulo_7;
+
+public class Aula7_8_tipo_Char {
+
+	public static void main(String[] args) {
+		
+		/*CHAR representa uma letra*/
+		char pessoaFeminino = 'F';
+		 
+		if(pessoaFeminino == 'f' || pessoaFeminino == 'F') {
+			System.out.println("Feminino");
+		}else {
+			System.out.println("Masculino");
+		}
+
+	}
+
+}
