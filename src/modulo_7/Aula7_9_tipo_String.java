be30e9c5c9d9dfc6c2a9aaf34509ba2650587ca5
AVELLG1713MAX <AVELLG1713MAX@MarcellRP.mshome.net>
2022-01-28 19:34:51 -0300

    adicionando a aula 7.9 o tipo String de dados

--- src/modulo_7/Aula7_9_tipo_String.java
+++ src/modulo_7/Aula7_9_tipo_String.java
@@ -4,6 +4,10 @@ public class Aula7_9_tipo_String {
 
 	public static void main(String[] args) {
 		
+		
+		/*o tipo string no java armazena de letras a textos inteiros */
+		String texto = "Olá mundo";
+		
 
 	}
 
