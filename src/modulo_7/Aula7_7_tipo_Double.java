b173a21036a66f699b2a240f614b0a91646ed025
AVELLG1713MAX <AVELLG1713MAX@MarcellRP.mshome.net>
2022-01-28 19:06:20 -0300

    adicionando aula 7.7 o tipo double de dados

--- src/modulo_7/Aula7_7_tipo_Double.java
+++ src/modulo_7/Aula7_7_tipo_Double.java
@@ -3,7 +3,14 @@ package modulo_7;
 public class Aula7_7_tipo_Double {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		double nota4 = 83;
+		
+		double media = ((nota1 + nota2 + nota3 + nota4) / 4) ;
+		
+	  System.out.println("Média das notas é: "+ media);
 
 	}
 
