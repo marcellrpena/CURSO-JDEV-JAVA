7b32de671b07881317889a7e6348c404b89496b0
AVELLG1713MAX <AVELLG1713MAX@MarcellRP.mshome.net>
2022-01-28 18:49:52 -0300

    adicionando a aula 7.6 fazendo operações com numeros inteiros

---
+++ src/modulo_7/Aula_7_6_operacao_numeros_int.java
@@ -0,0 +1,18 @@
+package modulo_7;
+
+public class Aula_7_6_operacao_numeros_int {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		var media = (nota1 + nota2 + nota3 + nota4)/4 ;
+		
+	  System.out.println("Média das notas é: "+ media);
+
+	}
+
+}
